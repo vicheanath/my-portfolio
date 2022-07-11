67f0a6b0a07b3027716dd3db6598604e823ab5c0
Vichea Nath <nathvichea1@gmail.com>
2022-07-11 15:22:21 +0700

    add image

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,22 @@ import { gsap, TweenMax, Power3 } from 'gsap'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { useRef, useEffect, useState } from 'react'
 import { isServer } from '../libs/isServer'
+import { TimelineLite, Power2 } from "gsap";
 
 const Home: NextPage = () => {
   let router = useRouter()
-  let name = useRef<HTMLHeadingElement>(null)
-  let TitleOne = useRef<HTMLHeadingElement>(null)
-  let TitleTwo = useRef<HTMLHeadingElement>(null)
+  let name = useRef<HTMLDivElement | null>(null)
+  let TitleOne = useRef<HTMLDivElement | null>(null)
+  let TitleTwo = useRef<HTMLDivElement | null>(null)
+
+  let image = useRef<HTMLDivElement | null>(null);
+  let container = useRef<HTMLDivElement | null>(null);
+  let imageReveal = useRef<HTMLDivElement | null>(null);
+  let tl = new TimelineLite();
+
   useEffect(() => {
     if (isServer) return
     gsap.from([name, TitleOne, TitleTwo], .8, {
@@ -25,27 +31,44 @@ const Home: NextPage = () => {
     })
   }, [name, TitleOne, TitleTwo])
 
-
   return (
-    <div className="wrapper">
-      <main className="main">
-        <div className="main__txt-wrap">
-          <h4 className="name">
-            <span ref={el => name = el} className="wrap-hidden">Vichea Nath</span>
-          </h4>
-          <h1>
-            <span ref={el => TitleOne = el} className="wrap-hidden">
-              Software Engineer
-            </span>
-          </h1>
-          <h1 className="ux-ui">
-            <span ref={el => TitleTwo = el} className="wrap-hidden">
-              UX/UI Developer
-            </span>
-          </h1>
-        </div>
-      </main>
-    </div>
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+      <div className="wrapper">
+        <main className="main">
+          <div className="container" ref={el => (container = el)}>
+            <div className="profile" ref={el => (image = el)} >
+              <Image
+                className='hoverableArea'
+                src="/image/angkor.jpg"
+                alt="logo"
+                width={900}
+                height={500}
+                layout="responsive" 
+                objectFit="cover"
+                /> 
+            </div>
+          </div>
+          <div className="main__txt-wrap">
+            <h4 className="name">
+              <span ref={el => name = el} className="wrap-hidden">Vichea Nath</span>
+            </h4>
+            <h1>
+              <span ref={el => TitleOne = el} className="wrap-hidden">
+                Software Engineer
+              </span>
+            </h1>
+            <h1 className="ux-ui">
+              <span ref={el => TitleTwo = el} className="wrap-hidden">
+                UX/UI Developer
+              </span>
+            </h1>
+          </div>
+        </main>
+      </div>
+    </>
   )
 }
 
